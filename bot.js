bde75fdf00f998c98bd4c544faf48fc6292f4c66
supermike444 <hoellemj@mail.uc.edu>
2016-11-16 15:22:48 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,16 +33,16 @@ function postMessage(wew) {
   var split = request.split("");
   var multiple = Number(split[0])*Number(split[2]);
   var i;
-  var array;
+  var array = [];
   var wew = Number(split[0]);
   var lad = Number(split[2]);
   for(i = 0; i < wew; i++) {
   array.push(Math.floor(Math.random()*lad) + 1);
   }
-//   var sum = array.reduce(add, 0);
-//  function add(a,b) {
-//  return a + b;
-//  }
+  var sum = array.reduce(add, 0);
+  function add(a,b) {
+  return a + b;
+  }
   
   botResponse = wew;
 
