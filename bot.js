a914a9f79bd6189fe74eacda4636e4075be0d4d3
supermike444 <hoellemj@mail.uc.edu>
2016-11-16 11:20:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]);
-  var regex = /^\d(?=d)$/;
+  var regex = /^\dd\d$/;
   
   postMessage();
   /*
